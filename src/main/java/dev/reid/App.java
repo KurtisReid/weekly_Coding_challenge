0d7a7bc5e7704c634ffa3fa4c72f13b08fc8dfc0
Kurtis <kreid8@kent.edu>
2022-10-20 11:08:44 -0400

    Initial commit

---
+++ src/main/java/dev/reid/App.java
@@ -0,0 +1,47 @@
+package dev.reid;
+
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) {
+
+        /*
+        \
+        Given two singly linked lists that intersect at some point, find the intersecting node. Assume the lists are non-cyclical.
+
+For example, given A = 3 ➔ 7 ➔ 8 ➔ 10 and B = 99 ➔ 1 ➔ 8 ➔ 10, return the node with value 8. In this example, assume nodes with the same value are the exact same node objects.
+
+Do this in 0( m + n) time (where m and n are the lengths of the lists) and constant space.
+         */
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>();
+
+        l1.add(3);
+        l1.add(7);
+        l1.add(8);
+        l1.add(10);
+
+        l2.add(99);
+        l2.add(1);
+        l2.add(8);
+        l2.add(10);
+
+        for (int i = 0; i < l1.size(); i++)
+        {
+            for (int x = 0; x < l2.size(); x++)
+            {
+                if (l1.get(i) == l2.get(x))
+                {
+                    System.out.println("l1 == l2");
+                    i = l1.size() + 1000;
+                    x = l2.size() + 1000;
+                }
+            }
+        }
+
+
+
+
+
+     }
+}
---
+++ src/main/java/dev/reid/stk/KurtStack.java
@@ -0,0 +1,54 @@
+package dev.reid.stk;
+
+import java.util.Queue;
+//LIFO
+public class KurtStack {
+     //Integer[] arr = new Integer[0];
+     //Integer[] maxStack = new Integer[0];
+    private class Node {
+        int data;
+        Node nlink;
+     }
+     Node top;
+     int max = 0;
+     Node maxPlace = new Node();
+     int currentPointer = 0;
+
+    public KurtStack(Node n) {
+        this.top = n;
+        maxPlace = n;
+    }
+
+    public void push(int val)
+    {
+        Node temp = new Node();
+        temp.data = val;
+        temp.nlink = top;
+        top = temp;
+        if (val > maxPlace.data)
+        {
+            max = val;
+            maxPlace = temp;
+        }
+
+
+
+    }
+    public int pop()
+    {
+        if (top == null)
+        {
+            System.out.println("top is null");
+            return -1;
+        }
+        Node temp = top;
+        top = top.nlink;
+        return temp.data;
+    }
+    public int max()
+    {
+        return maxPlace.data;
+    }
+
+
+}
